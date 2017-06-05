bad98e20256110e2b70d03167fe28b7ff70ae5a3
levineh@msn.com <levineh@msn.com>
2017-06-05 07:46:37 -0400

    Working initial version

---
+++ ReverseSentence/ConsoleApplication1/Reverser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Reverser
+    {
+        public static void ReverseSentence()
+        {
+            string result = "";
+            string input = "";
+            //Char.IsLetter(ch)
+            Console.WriteLine("Enter a sentence to reverse");
+            input = Console.ReadLine();
+            string[] stringArray = input.Split(' ');
+            Array.Reverse(stringArray);
+            result = string.Join(" ", stringArray);
+            Console.WriteLine(result);
+        }
+
+    }
+}
